a0469a2240fbf6991d0c5beb3e7cdbcb65da0c4b
Thomas Linkin <trlinkin@gmail.com>
2013-03-10 20:07:43 -0400

    Initial commit: Creation of new fact. This is the initial POC for this fact, I hope to make it slightly more funtional later on. Possbily break out Admin users vs regular users.

---
+++ lib/facter/windows_users.rb
@@ -0,0 +1,18 @@
+require 'Win32ole'
+
+Facter.add(:windows_users) do
+  confine :kernel => 'Windows'
+
+  setcode do
+    begin
+      wmi = WIN32OLE.connect("winmgmts:{impersonationLevel=impersonate}!//./root/cimv2")
+      wql = wmi.execquery("select * from win32_useraccount")
+
+      users = []
+      wql.each do |u|
+        users << u.name
+      end
+      users.join(",")
+    end
+  end
+end
