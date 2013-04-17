9dc7f466675462e2fe95aac7f8eddde24e97b7ce
Thomas Linkin <trlinkin@gmail.com>
2013-04-17 11:03:12 -0700

    Fact is now using the utility functions of Facter. Facter itself provides a utility function for performing WMI queries (otherwise known as WQL). This commit uses that utility function instead of creating the WMI connection itself.

--- lib/facter/windows_users.rb
+++ lib/facter/windows_users.rb
@@ -1,18 +1,15 @@
-require 'win32ole'
+require 'facter/util/wmi'
 
 Facter.add(:windows_users) do
   confine :kernel => 'Windows'
 
   setcode do
-    begin
-      wmi = WIN32OLE.connect("winmgmts:{impersonationLevel=impersonate}!//./root/cimv2")
-      wql = wmi.execquery("select * from win32_useraccount")
+    wql = Facter::Util::WMI.execquery("select * from win32_useraccount")
 
-      users = []
-      wql.each do |u|
-        users << u.name
-      end
-      users.join(",")
+    users = []
+    wql.each do |u|
+      users << u.name
     end
+    users.join(",")
   end
 end
